776800b4872ed9a8ab828faceaea075853470606
Krishnendu Dey <krishnendujdey@gmail.com>
2025-03-24 00:37:51 +0530

    Added functionality of creating posts, now displayed on the homepage.

--- index.js
+++ index.js
@@ -4,10 +4,34 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+// post array
+let posts = []
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/edit", (req, res) => {
+    res.render("edit.ejs", {
+        allPosts: posts
+    })
+})
+
+app.get("/new", (req, res) => {
+    res.render("new.ejs")
+})
+
+app.post("/newsubmit", (req, res) => {
+    let title = req.body.title
+    let content = req.body.content
+    const post = { title: title, content: content }
+    posts.push(post)
+    console.log(posts)
+    res.redirect("/")
+})
+
 app.get("/", (req, res) => {
-    res.render("index.ejs")
+    res.render("index.ejs", {
+        allPosts: posts
+    })
 });
 
 app.listen(port, () => {
